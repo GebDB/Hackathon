b8d09e47f99dfcfb4ffa91f0bc975f0e74dda902
GebDB <becirevicdaniel@gmail.com>
2023-11-05 01:46:22 -0600

    all cards 4x1 column needing to be fixed

--- step-in-business/src/pages/Home.js
+++ step-in-business/src/pages/Home.js
@@ -1,19 +1,41 @@
 import React from 'react';
 import './Home.css';
 import restaurantImage from './images/jay-wennington-N_Y88TWmGwA-unsplash.jpg';
+import healthcareImage from './images/jeshoots-com-l0j0DHVWcIE-unsplash.jpg';
+import goodsImage from './images/luma-candles-K1Xi_R_mfpk-unsplash.jpg'
+import servicesImage from './images/emmanuel-ikwuegbu-_2AlIm-F6pw-unsplash.jpg'
 export const Home = () => {
+  const CardsData = [
+    {
+      title: 'Restaurant',
+      image: restaurantImage,
+    },
+    {
+      title: 'Healthcare',
+      image: healthcareImage,
+    },
+    {
+      title: 'Goods',
+      image: goodsImage,
+    },
+    {
+      title: 'Services',
+      image: servicesImage,
+    }
+  ];
+
   return (
-    <div className="Home-right-container">
-      <h1 className="Click-an-industry"> Click an industry!</h1>
-        
-    <div className="Restaurant-card">
-        <h4 className="Restaurant-title">Restaurant</h4>
-          <img className="Restaurant-Image"
-            src={restaurantImage}
-            alt="Restaurant"
-          />      
-        </div>
+  <div className="Home-right-container">
+    <h1 className="Click-an-industry"> Click an industry!</h1>
+    <div className="card-container">
+      {CardsData.map((card,index)=> (
+        <div className="industry-card" key={index}>
+          <h4 className="industry-title">{card.title}</h4>
+          <img className="industry-image" src={card.image}alt={card.title} />
     </div>
+  ))}
+  </div>
+</div>
   );
-}
+};
 export default Home;

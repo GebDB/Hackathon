4a4480fdefce6d26ff049361dc0339c24289690a
GebDB <becirevicdaniel@gmail.com>
2023-11-05 01:23:57 -0600

    Restaurant base card added

--- step-in-business/src/pages/Home.js
+++ step-in-business/src/pages/Home.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import './Home.css';
+import restaurantImage from './images/jay-wennington-N_Y88TWmGwA-unsplash.jpg';
 export const Home = () => {
   return (
-    <div className="Home-container">
-      <h1> Click an industry!</h1>
-        <div className="card">
-          <img
-            src="./img/restaurant_image.jpg"
+    <div className="Home-right-container">
+      <h1 className="Click-an-industry"> Click an industry!</h1>
+        
+    <div className="Restaurant-card">
+        <h4 className="Restaurant-title">Restaurant</h4>
+          <img className="Restaurant-Image"
+            src={restaurantImage}
             alt="Restaurant"
-          />
+          />      
         </div>
     </div>
   );
